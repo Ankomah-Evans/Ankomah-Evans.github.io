4f38e186d49feaf13949edeaa166a155d4ff845d
Ankomah-Evans <ankomahrich@gmail.com>
2017-07-13 10:38:49 -0600

    Mobile app

---
+++ Mobile App/header.php
@@ -0,0 +1,34 @@
+
+        <title>CIT 261 Mobile APP</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="visitcount.js"></script>
+    
+
+    <body onload="visitCounter();">
+    <h1>Practice and Master your simple Mathematical Equations</h1>
+        <div id="nav"> <br>
+            
+    <div id="nav_wrapper"><br>
+                
+        <ul class="navigation">
+            
+              <li><a href="ProjectApp.html" title="Mobile Application">Home</a></li>
+         
+  <li><a href="CountEven.html" title="Mobile Application">Addition</a></li>
+	<li><a href="CountEven.html" title="Mobile Application">Subtraction</a></li>
+   <li><a href="Investment Calculator.html" title="Mobile Application">Investment</a></li>   
+    <li><a href="Loan Calculator.html" title="Mobile Application">Trignometry</a></li>
+     <li><a href="Multiply Even.html" title="Mobile Application">Multiplication</a></li>
+       <li><a href="Tables.html" title="Mobile Application">Area</a></li>
+        <li><a href="Wind Cal.html" title="Mobile Application">Parameters</a></li>
+         <li><a href="integer.html" title="Mobile Application">Volume</a></li>
+         
+        </ul>
+    </div>
+       
+</div>
+        
+                <div>
+ <p> This Application enables a user to learn, master, and easily earn fast and correct answers in basic mathematics equations, just by computing them.</p>
